ca4f49f9e32d06b1e07edb48462862d12dc88799
azrappp <azraf.aziz@gmail.com>
2025-01-20 05:13:21 -0500

    feat: add overlay on banner

--- src/pages/Ideas.jsx
+++ src/pages/Ideas.jsx
@@ -19,19 +19,28 @@ const Ideas = () => {
   return (
     <>
       <div className="relative h-[50vh] overflow-hidden">
+        {/* Overlay Hitam dengan Segitiga */}
+        <div
+          className="absolute inset-0 bg-black opacity-50 z-10"
+          style={{
+            transform: `translateY(${offsetY * 0.5}px)`,
+            clipPath: "polygon(0 0, 100% 0%, 100% 80%, 0 100%)", // Sama seperti pada background
+          }}
+        ></div>
+
         {/* Background Image with Gradient Overlay */}
         <div
           className="absolute top-0 left-0 w-full h-full bg-cover bg-center bg-gradient-to-t from-black to-transparent"
           style={{
             backgroundImage: "url('/banner-ideas.webp')",
             transform: `translateY(${offsetY * 0.5}px)`,
-            clipPath: "polygon(0 0, 100% 0%, 100% 80%, 0 100%)", // Sesuaikan nilai ini
+            clipPath: "polygon(0 0, 100% 0%, 100% 80%, 0 100%)", // Sama seperti overlay hitam
           }}
         ></div>
 
         {/* Text Content */}
         <div
-          className="relative flex flex-col justify-center items-center text-white h-full bg-blend-overlay"
+          className="relative flex flex-col justify-center items-center text-white h-full z-20"
           style={{
             transform: `translateY(${offsetY * 0.3}px)`,
           }}
